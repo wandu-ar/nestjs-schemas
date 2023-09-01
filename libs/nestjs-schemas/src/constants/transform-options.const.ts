7b46dfa9c14f121b0071d7eb59df7616cadf909b
Alejandro D. Guevara <47149928+wandu-ar@users.noreply.github.com>
2023-09-01 17:21:04 -0300

    only bkp!

--- libs/nestjs-schemas/src/constants/transform-options.const.ts
+++ libs/nestjs-schemas/src/constants/transform-options.const.ts
@@ -5,5 +5,7 @@ export const defaultTransformOptions: ClassTransformOptions = {
   excludeExtraneousValues: true,
   enableImplicitConversion: true,
   exposeDefaultValues: true,
-  exposeUnsetFields: true,
+  // exposeUnsetFields anula Transform cuando se recibe un undefined
+  // Por lo que no se pueden crear props virtuales con Transforms
+  exposeUnsetFields: false,
 };
