1952cfac7f8f13d4e8049bb7ca504bd15bb38af5
Alejandro D. Guevara <47149928+wandu-ar@users.noreply.github.com>
2023-09-03 17:00:52 -0300

    fix opts

--- libs/nestjs-schemas/src/constants/transform-options.const.ts
+++ libs/nestjs-schemas/src/constants/transform-options.const.ts
@@ -6,6 +6,7 @@ export const defaultTransformOptions: ClassTransformOptions = {
   enableImplicitConversion: true,
   exposeUnsetFields: true,
   // exposeUnsetFields anula Transform cuando se recibe un undefined
-  // Por lo que no se pueden crear props virtuales con Transforms
-  exposeDefaultValues: false,
+  // Por lo que no se pueden crear props virtuales con Transforms si el objeto el cual se pasa
+  // a plainToInstance no llega esa propiedad, por lo que debe injectarse antes
+  exposeDefaultValues: true,
 };
