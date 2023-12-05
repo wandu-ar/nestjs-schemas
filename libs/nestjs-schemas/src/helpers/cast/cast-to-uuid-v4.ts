17cd9e3d6bb94f74f21bdb3164764c3642ecc16d
Alejandro D. Guevara <47149928+wandu-ar@users.noreply.github.com>
2023-12-05 17:03:17 -0300

    fix cast to uuidv4 issue

--- libs/nestjs-schemas/src/helpers/cast/cast-to-uuid-v4.ts
+++ libs/nestjs-schemas/src/helpers/cast/cast-to-uuid-v4.ts
@@ -14,14 +14,20 @@ export function castToUUIDv4(value: any, options: CastToUUIDv4Options = {}) {
   return castTo(value, false, castToUUIDv4Fn, options, { ...options });
 }
 
-export function castToUUIDv4Array(value: any, options: CastToUUIDv4ArrayOptions = {}) {
+export function castToUUIDv4Array(
+  value: any,
+  options: CastToUUIDv4ArrayOptions = {},
+) {
   return castTo(value, true, castToUUIDv4Fn, options, { ...options });
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function castToUUIDv4Fn(value: any, options: CastToUUIDv4 = {}) {
   let newValue: UUIDv4;
-  if (typeof value === 'string' && isUUID(value, '4')) {
+  if (
+    typeof value === 'string' &&
+    (isUUID(value, '4') || value === '00000000-0000-0000-0000-000000000000')
+  ) {
     newValue = toUUIDv4(value);
   } else if (typeof value === 'object' && value instanceof Binary) {
     newValue = toUUIDv4(value);
